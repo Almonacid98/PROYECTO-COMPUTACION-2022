bbe3b120efb901c140fab84019c09d2588bdd390
Almonacid98 <gabriel98almonacid@hotmail.com>
2022-10-28 09:06:22 -0300

    Borrado temporal

--- GAME_SYSTEM_COMBAT/character_and_enemy_factory/enemy_factory/enemy_factory.py
+++
@@ -1,5 +0,0 @@
-from character_and_enemy_factory.character_factory.character_factory import Character
-class Enemy(Character):
-    def __init__(self, name, age, strength, agility, life, type) -> None:
-        super().__init__(name, age, strength, agility, life, type)                         #https://pythondiario.com/2016/10/simple-juego-rpg-en-python.html
-        
--- GAME_SYSTEM_COMBAT/visual_interface/interface/visual.py
+++
@@ -1,8 +0,0 @@
-
-def character_view():
-    a = {}
-    with open("C:/Users/devil/OneDrive/Escritorio/GAME_SYSTEM_COMBAT/archive_consult/character_one.txt") as f:
-        for line in f:
-            (k, v) = line.split()
-            a[int(k)] = v
-    print(a)
